61a2e0ff6640ff2b750a99f3424791bd6c5f4d58
virattt <virat.dot@gmail.com>
2013-08-05 21:16:14 -0700

    second commit

--- gen/com/example/hello_world/R.java
+++ gen/com/example/hello_world/R.java
@@ -24,27 +24,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dataView1=0x7f080001;
-        public static final int dataView2=0x7f080003;
-        public static final int dataView3=0x7f080005;
-        public static final int dataView4=0x7f080007;
-        public static final int dataView5=0x7f080009;
-        public static final int dataView6=0x7f08000b;
-        public static final int dataView7=0x7f08000d;
-        public static final int dataView8=0x7f08000f;
-        public static final int menu_add=0x7f080010;
-        public static final int menu_attack=0x7f080013;
-        public static final int menu_config=0x7f080011;
-        public static final int menu_home=0x7f080014;
-        public static final int menu_travel=0x7f080012;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080004;
-        public static final int textView4=0x7f080006;
-        public static final int textView5=0x7f080008;
-        public static final int textView6=0x7f08000a;
-        public static final int textView7=0x7f08000c;
-        public static final int textView8=0x7f08000e;
+        public static final int dataView1=0x7f080007;
+        public static final int dataView2=0x7f080009;
+        public static final int dataView3=0x7f08000b;
+        public static final int dataView4=0x7f08000d;
+        public static final int dataView5=0x7f08000f;
+        public static final int dataView6=0x7f080011;
+        public static final int dataView7=0x7f080013;
+        public static final int dataView8=0x7f080015;
+        public static final int imageEarth=0x7f080000;
+        public static final int imageJupiter=0x7f080003;
+        public static final int imageMars=0x7f080001;
+        public static final int imageNeptune=0x7f080005;
+        public static final int imageSaturn=0x7f080004;
+        public static final int imageVenus=0x7f080002;
+        public static final int menu_add=0x7f080016;
+        public static final int menu_attack=0x7f080019;
+        public static final int menu_config=0x7f080017;
+        public static final int menu_home=0x7f08001a;
+        public static final int menu_travel=0x7f080018;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080008;
+        public static final int textView3=0x7f08000a;
+        public static final int textView4=0x7f08000c;
+        public static final int textView5=0x7f08000e;
+        public static final int textView6=0x7f080010;
+        public static final int textView7=0x7f080012;
+        public static final int textView8=0x7f080014;
     }
     public static final class layout {
         public static final int activity_add=0x7f030000;
@@ -56,6 +62,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f05000f;
         public static final int app_name=0x7f050000;
+        public static final int content_desc_earth=0x7f050010;
+        public static final int content_desc_jupiter=0x7f050013;
+        public static final int content_desc_mars=0x7f050011;
+        public static final int content_desc_neptune=0x7f050015;
+        public static final int content_desc_saturn=0x7f050014;
+        public static final int content_desc_venus=0x7f050012;
         public static final int hello_world=0x7f050001;
         public static final int menu_add_planet=0x7f05000a;
         public static final int menu_attack_planet=0x7f05000d;
